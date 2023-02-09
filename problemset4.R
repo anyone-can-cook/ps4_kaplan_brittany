070494d92f4133cdff9c118650b974d721db5dfd
Brittany Kaplan <bkaplan@MacBook-Pro-70.local>
2023-02-08 18:59:23 -0800

    Committing changes in problemset4.R

--- problemset4.R
+++ problemset4.R
@@ -1,1 +1,6 @@
-#Brittany Kaplan
+library(tidyverse)
+mrc <- read.csv(download.file(url = 'http://www.equality-of-opportunity.org/data/college/mrc_table2.csv', 
+                destfile = ("." , "data")))mrc_pivot<- mrc_subset %>% 
+  pivot_longer(cols = 'par_q1':'par_q5',
+               names_to = 'quantile',
+               values_to = 'fraction')
