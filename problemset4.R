4f0bed75fb73e3cc9a2712b3221cd76cf006c91c
Brittany Kaplan <bkaplan@MacBook-Pro-70.local>
2023-02-08 19:03:00 -0800

    Committing changes in problemset4.R

--- problemset4.R
+++ problemset4.R
@@ -1,6 +1,7 @@
 library(tidyverse)
-mrc <- read.csv(download.file(url = 'http://www.equality-of-opportunity.org/data/college/mrc_table2.csv', 
-                destfile = ("." , "data")))mrc_pivot<- mrc_subset %>% 
+mrc <- read_csv(file = 'http://www.equality-of-opportunity.org/data/college/mrc_table2.csv')
+mrc_subset <- subset(mrc, select = c(name, par_q1, par_q2, par_q3, par_q4, par_q5))
+mrc_pivot<- mrc_subset %>% 
   pivot_longer(cols = 'par_q1':'par_q5',
                names_to = 'quantile',
                values_to = 'fraction')
